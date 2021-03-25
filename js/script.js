a66bb2ac2ce29963b885008a9382c647ff4ba1f0
Daria <daria.citak1703@gmail.com>
2021-03-25 22:18:41 +0100

    Quiz

--- js/script.js
+++ js/script.js
@@ -172,9 +172,16 @@ let preQuestions =
         }];
 
 let next = document.querySelector('.next');
+let previous = document.querySelector('.previous');
+
+console.log(next);
+console.log(previous);
 
 let question = document.querySelector('.question');
 let answers = document.querySelectorAll('.list-group-item');
+
+console.log(answers);
+
 
 let pointsElem = document.querySelector('.score');
 let restart = document.querySelector('.restart');
@@ -185,10 +192,34 @@ for (let i = 0; i < answers.length; i++) {
     answers[i].addEventListener('click', doAction);
 }
 
+function setQuestion(index) {
+	
+	enableAnswers();
+	clearAnswers;
+   
+   if (preQuestions[index].answers.length === 2) {
+       answers[2].style.display = 'none';
+       answers[3].style.display = 'none';
+   } else {
+       answers[2].style.display = 'block';
+       answers[3].style.display = 'block';
+   }
+
+   question.innerHTML = preQuestions[index].question;
+
+   answers[0].innerHTML = preQuestions[index].answers[0];
+   answers[1].innerHTML = preQuestions[index].answers[1];
+   answers[2].innerHTML = preQuestions[index].answers[2];
+   answers[3].innerHTML = preQuestions[index].answers[3];
+   
+}
+
+
+
 function doAction(event) {
     //event.target - Zwraca referencję do elementu, do którego zdarzenie zostało pierwotnie wysłane.
     if (event.target.innerHTML === preQuestions[index].correct_answer) {
-        points++;
+        points++; 
         pointsElem.innerText = points;
         markCorrect(event.target);
     }
@@ -198,6 +229,21 @@ function doAction(event) {
     disableAnswers();
 }
 
+next.addEventListener('click', function () {
+   ++index;
+   setQuestion(index);
+   //activateAnswers();
+});
+
+previous.addEventListener('click', function () {
+   --index;
+   
+   if (index < 0){
+	   index = 0;
+	   return;
+   }
+   setQuestion(index);
+});
 
 
 restart.addEventListener('click', function (event) {
@@ -211,4 +257,61 @@ restart.addEventListener('click', function (event) {
     activateAnswers();
     list.style.display = 'block';
     results.style.display = 'none';
+	
 });
+
+function enableAnswers(){
+	for (let i = 0; i < answers.length; i++){
+		answers[i].addEventListener('click', check)
+	}
+}
+
+function disableAnswers(){
+	for (let i = 0; i < answers.length; i++){
+		answers[i].removeEventListener('click', check)
+	}
+}
+
+enableAnswers();
+
+
+
+function check(event){
+	if (event.target.innerText === preQuestions[index].correct_answer){
+		markCorrect(event.target);
+	} else{
+		markInCorrect(event.target);
+	}
+	disableAnswers();
+		
+	
+}
+
+
+function markCorrect(elem){
+	elem.classList.add('correct');
+}
+
+function markInCorrect(elem){
+	elem.classList.add('incorrect');
+}
+
+function clearAnswers(){
+	
+	for(let i = 0; i < answers.length; i++){
+		let elem = answers[i];
+		if (elem.classList.contains('correct')){
+			elem.classList.remove('correct');
+		}
+		
+		else if (elem.classList.contains('incorrect')){
+			elem.classList.remove('incorrect');
+		}
+		
+	}
+}
+
+
+
+
+
